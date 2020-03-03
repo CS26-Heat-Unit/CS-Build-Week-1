b6beff7d6e5519757b634280a193b2c913be3a5d
Adam Payne <ajpayne238@gmail.com>
2020-03-03 18:18:07 -0500

    commented out dotenv_file

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -19,9 +19,9 @@ import dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+# dotenv_file = os.path.join(BASE_DIR, ".env")
+# if os.path.isfile(dotenv_file):
+    # dotenv.load_dotenv(dotenv_file)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
